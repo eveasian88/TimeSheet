d5a83b9493d86d46ab276dbeea0beb4230bed5e9
Susye Weng-Reeder <weng-reeder@hotmail.com>
2018-10-27 13:54:36 -0700

    wip

--- timesheet.js
+++ timesheet.js
@@ -1,34 +1,34 @@
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyBb_VvgidrYsaUud7zcbTIDCthA_EfNwPo",
     authDomain: "demoproject-40b0e.firebaseapp.com",
     databaseURL: "https://demoproject-40b0e.firebaseio.com",
     projectId: "demoproject-40b0e",
     storageBucket: "demoproject-40b0e.appspot.com",
     messagingSenderId: "100672990588"
-  };
-  firebase.initializeApp(config);
- 
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
 
-  //initial values
-  var name = "";
-  var role = "";
-  var startDate = "";
-  var monthsWorked = "";
-  var monthlyRate = "";
-  var totalBill = "";
+var database = firebase.database();
 
-  // capture button click
-$("#add-user").on("click", function(event) {
-// don't refresh page
-event.preventDefault();
+//initial values
+var name = "";
+var role = "";
+var startDate = "";
+var monthsWorked = "";
+var monthlyRate = "";
+var totalBill = "";
 
-name = $("#name-input").val().trim();
-role = $("#role-input").val().trim();
-startDate = $("#startdate-input").val().trim();
-monthlyRate = $("#rate-input").val().trim();
+// capture button click
+$("#add-user").on("click", function (event) {
+    // don't refresh page
+    event.preventDefault();
+
+    name = $("#name-input").val().trim();
+    role = $("#role-input").val().trim();
+    startDate = $("#startdate-input").val().trim();
+    monthlyRate = $("#rate-input").val().trim();
 
     database.ref().push({
         name: name,
@@ -38,17 +38,21 @@ monthlyRate = $("#rate-input").val().trim();
         dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
-    //firebase watcher + initial loader HINT
-// DataTransfer.ref().on("child_added", function(childSnapshot) {
+    firebase watcher + initial loader HINT
+    DataTransfer.ref().on("child_added", function (childSnapshot) {
 
-//     console.log("childSnapshot", childSnapshot.val());
+        console.log("childSnapshot", childSnapshot.val());
 
-//     console.log(childSnapshot.val().name);
-//     console.log(childSnapshot.val().role);
-//     console.log(childSnapshot.val().startDate);
-//     console.log(childSnapshot.val().monthlyRate);
+        console.log(childSnapshot.val().name);
+        console.log(childSnapshot.val().role);
+        console.log(childSnapshot.val().startDate);
+        console.log(childSnapshot.val().monthlyRate);
 
-// })/timesheet.js
+    })
+
+    var randomDate = "11/19/2016";
+    var formated = "MM/DD/YYYY";
+    var convertedDate = moment(randomDate, formated);
 
 });
 
